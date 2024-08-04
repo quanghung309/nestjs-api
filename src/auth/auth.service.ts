8d057ed1096cfef7f81606af36706b12773b9583
quanghung309 <95295846+quanghung309@users.noreply.github.com>
2024-08-04 10:10:35 +0700

    Fix model and custom package json

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -9,21 +10,43 @@ export class AuthService {
     //! generate the password hash
     const hash = await argon.hash(dto.password);
     //! save the new user to the database
-    const user = await this.prisma.user.create({
-      data: {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        //! the code of duplicate key error
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      } else {
+        throw error;
+      }
+    }
+
+    //! return the saved user
+  }
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
         email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
       },
     });
-    //! return the saved user
+    // if user does not exist, throw an error
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    // compare the password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if the password is incorrect, throw an error
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    // send back the user
+    delete user.hash;
     return user;
-  }
-  signin() {
-    return { message: 'I am sign in' };
   }
 }
