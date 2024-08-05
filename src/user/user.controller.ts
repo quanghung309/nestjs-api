491a5e8754ef552be53e8716f7101d9071114a7f
quanghung309 <95295846+quanghung309@users.noreply.github.com>
2024-08-05 16:47:46 +0700

    Authentication user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('user')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+}
